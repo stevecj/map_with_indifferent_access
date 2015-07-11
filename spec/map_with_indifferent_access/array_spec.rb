472968e21fbdfccff205fc69c056f86a72a1dabe
Steve Jorgensen <stevej@stevej.name>
2015-07-11 14:05:00 -0700

    Clean up specs a bit

--- spec/map_with_indifferent_access/array_spec.rb
+++ spec/map_with_indifferent_access/array_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe MapWithIndifferentAccess::Array do
-  subject { described_class.new( array ) }
-  let( :array ) { [] }
+  let( :inner_array ) { subject.inner_array }
 
   it "can be constructed as a wrapper around an existing array" do
     inner_a = []
@@ -30,8 +29,8 @@ describe MapWithIndifferentAccess::Array do
   end
 
   it "can be converted from an instance of its class, returning the given array" do
-    map = described_class::try_convert( subject )
-    expect( map ).to equal( subject )
+    array = described_class::try_convert( subject )
+    expect( array ).to equal( subject )
   end
 
   it "cannot be converted from an un-array-like object" do
@@ -41,32 +40,32 @@ describe MapWithIndifferentAccess::Array do
   end
 
   it "can be converted from an array, wrapping the given array" do
-    array = []
+    given_array = []
 
-    hwia_array = described_class::try_convert( array )
+    array = described_class::try_convert( given_array )
 
-    expect( hwia_array ).to be_kind_of( described_class)
-    expect( hwia_array.inner_array ).to equal( array )
+    expect( array ).to be_kind_of( described_class)
+    expect( array.inner_array ).to equal( given_array )
   end
 
   it "delegates storing an item by index" do
     subject[3] = :abc
-    expect( array[3] ).to eq( :abc )
+    expect( inner_array[3] ).to eq( :abc )
   end
 
   it "delegates reading an item by index" do
-    array[2] = :abc
+    inner_array[2] = :abc
     expect( subject[2] ).to eq( :abc )
   end
 
   it "reflects hash-type item by index as wrapped" do
-    array[3] = {a: 1}
+    inner_array[3] = {a: 1}
     expect( subject[3] ).to be_kind_of( MapWithIndifferentAccess )
     expect( subject[3].inner_map ).to eq( {a: 1} )
   end
 
   it "reflects array-type item by index as wrapped" do
-    array[3] = [:a, :b]
+    inner_array[3] = [:a, :b]
     expect( subject[3] ).to be_kind_of( described_class )
   end
 
--- spec/map_with_indifferent_access_spec.rb
+++ spec/map_with_indifferent_access_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe MapWithIndifferentAccess do
+  let( :inner_map ) { subject.inner_map }
+
   it 'has a version number' do
     expect(MapWithIndifferentAccess::VERSION).not_to be nil
   end
@@ -50,135 +52,129 @@ describe MapWithIndifferentAccess do
     expect( map.inner_map ).to equal( hash )
   end
 
-  context "an instance constructed with no arguments" do
-    it "Allows indexed read/write access to values" do
-      subject[  1  ] = 'one'
-      subject[ 'a' ] = 'A'
-      subject[ :b  ] = 'B'
-      expect( subject[  1  ] ).to eq( 'one' )
-      expect( subject[ 'a' ] ).to eq( 'A'   )
-      expect( subject[ :b  ] ).to eq( 'B'   )
-    end
-
-    it "treats string and symbol keys interchangeably" do
-      subject[ 'a' ] = 'A'
-      subject[ :b  ] = 'B'
-      expect( subject[ :a  ] ).to eq( 'A' )
-      expect( subject[ 'b' ] ).to eq( 'B' )
-    end
-
-    it "provides Hash-like #fetch behavior with key symbol/string indifference" do
-      subject[  1  ] = 'one'
-      subject[ 'a' ] = 'A'
-      subject[ :b  ] = 'B'
-      expect( subject.fetch(  1  ) ).to eq( 'one' )
-      expect( subject.fetch( :a  ) ).to eq( 'A'   )
-      expect( subject.fetch( 'b' ) ).to eq( 'B'   )
-
-      expect{ subject.fetch('x') }.to raise_exception( KeyError )
-      expect( subject.fetch('x') { '-' } ).to eq( '-' )
-      expect( subject.fetch('x', '#') ).to eq( '#' )
-    end
-
-    it "provides its length/size" do
-      subject[  1  ] = 'one'
-      subject[ 'a' ] = 'A'
-      expect( subject.length ).to eq( 2 )
-      expect( subject.size   ).to eq( 2 )
-    end
-
-    it "enumerates keys in order added" do
-      subject[  1     ] = 1
-      subject[ 'two'  ] = 2
-      subject[ :three ] = 3
-
-      keys = []
-      subject.each_key do |key| ; keys << key ; end
-      expect( keys ).to eq( [1, 'two', :three] )
-    end
-
-    it "has unequal instances via #== with key-symbol-string-indifferently unequal entry sets" do
-      subject[  1     ] = 1
-      subject[ 'two'  ] = [ {a: 4} ]
-      subject[ :three ] = 3
-
-      other = described_class.new
-      other[ 'three' ] = 3
-      other[ :two    ] = [ {a: :fore} ]
-      other[  1      ] = 1
-
-      expect( subject == other ).to eq( false )
-    end
-
-    it "has equal instances via #== with key-symbol-string-indifferently equal entry sets" do
-      subject[  1     ] = 1
-      subject[ 'two'  ] = 2
-      subject[ :three ] = 3
-
-      other = described_class.new
-      other[ 'three' ] = 3
-      other[ :two    ] = 2
-      other[  1      ] = 1
-
-      expect( subject == other ).to eq( true )
-    end
-
-    it "is enumerable over key/value pairs" do
-      subject[  1     ] = 1
-      subject[ 'two'  ] = { a: 1 }
-      subject[ :three ] = [ 9 ]
-
-      entries = []
-      subject.each do |entry| ; entries << entry ; end
-
-      expect( entries ).to eq( [
-        [  1,     1 ],
-        [ 'two',  described_class.new(a: 1) ],
-        [ :three, described_class::Array.new([9]) ]
-      ] )
-
-      expect( subject.entries ).to eq( entries )
-    end
+  it "Allows indexed read/write access to values" do
+    subject[  1  ] = 'one'
+    subject[ 'a' ] = 'A'
+    subject[ :b  ] = 'B'
+    expect( subject[  1  ] ).to eq( 'one' )
+    expect( subject[ 'a' ] ).to eq( 'A'   )
+    expect( subject[ :b  ] ).to eq( 'B'   )
   end
 
-  context "An instance constructed with an existing Hash" do
-    subject { described_class.new(hash) }
-    let( :hash ) { {} }
-
-    it "reflects later changes made to the existing hash" do
-      hash[ :foo ] = 123
-      expect( subject['foo'] ).to eq( 123 )
-    end
-
-    it "reflects later changes back to the existing hash" do
-      subject[ :abc ] = 'ABC'
-      expect( hash[:abc] ).to eq('ABC')
-    end
-
-    it "reflects hash-type values from the existing hash as wrapped" do
-      h = {}
-      hash[:aaa] = h
-      expect( subject[:aaa] ).to be_kind_of( described_class )
-      expect( subject[:aaa].inner_map ).to equal( h )
-    end
-
-    it "reflects array-type values from the existing hash as wrapped" do
-      ary = []
-      hash[:bbb] = ary
-      expect( subject[:bbb] ).to be_kind_of( described_class::Array )
-      expect( subject[:bbb].inner_array ).to equal( ary )
-    end
-
-    it "modifies the existing underlying entry with string/symbol indifference" do
-      hash[ :aaa  ] = 'A'
-      hash[ 'bbb' ] = 'B'
-      subject[ 'aaa' ] = 'AA'
-      subject[ :bbb  ] = 'BB'
-
-      expect( hash ).to eq( {
-        :aaa  => 'AA',
-        'bbb' => 'BB'
-      } )
-    end
+  it "treats string and symbol keys interchangeably" do
+    subject[ 'a' ] = 'A'
+    subject[ :b  ] = 'B'
+    expect( subject[ :a  ] ).to eq( 'A' )
+    expect( subject[ 'b' ] ).to eq( 'B' )
   end
+
+  it "provides Hash-like #fetch behavior with key symbol/string indifference" do
+    subject[  1  ] = 'one'
+    subject[ 'a' ] = 'A'
+    subject[ :b  ] = 'B'
+    expect( subject.fetch(  1  ) ).to eq( 'one' )
+    expect( subject.fetch( :a  ) ).to eq( 'A'   )
+    expect( subject.fetch( 'b' ) ).to eq( 'B'   )
+
+    expect{ subject.fetch('x') }.to raise_exception( KeyError )
+    expect( subject.fetch('x') { '-' } ).to eq( '-' )
+    expect( subject.fetch('x', '#') ).to eq( '#' )
+  end
+
+  it "provides its length/size" do
+    subject[  1  ] = 'one'
+    subject[ 'a' ] = 'A'
+    expect( subject.length ).to eq( 2 )
+    expect( subject.size   ).to eq( 2 )
+  end
+
+  it "enumerates keys in order added" do
+    subject[  1     ] = 1
+    subject[ 'two'  ] = 2
+    subject[ :three ] = 3
+
+    keys = []
+    subject.each_key do |key| ; keys << key ; end
+    expect( keys ).to eq( [1, 'two', :three] )
+  end
+
+  it "has unequal instances via #== with key-symbol-string-indifferently unequal entry sets" do
+    subject[  1     ] = 1
+    subject[ 'two'  ] = [ {a: 4} ]
+    subject[ :three ] = 3
+
+    other = described_class.new
+    other[ 'three' ] = 3
+    other[ :two    ] = [ {a: :fore} ]
+    other[  1      ] = 1
+
+    expect( subject == other ).to eq( false )
+  end
+
+  it "has equal instances via #== with key-symbol-string-indifferently equal entry sets" do
+    subject[  1     ] = 1
+    subject[ 'two'  ] = 2
+    subject[ :three ] = 3
+
+    other = described_class.new
+    other[ 'three' ] = 3
+    other[ :two    ] = 2
+    other[  1      ] = 1
+
+    expect( subject == other ).to eq( true )
+  end
+
+  it "is enumerable over key/value pairs" do
+    subject[  1     ] = 1
+    subject[ 'two'  ] = { a: 1 }
+    subject[ :three ] = [ 9 ]
+
+    entries = []
+    subject.each do |entry| ; entries << entry ; end
+
+    expect( entries ).to eq( [
+      [  1,     1 ],
+      [ 'two',  described_class.new(a: 1) ],
+      [ :three, described_class::Array.new([9]) ]
+    ] )
+
+    expect( subject.entries ).to eq( entries )
+  end
+
+  it "reflects later changes made to the existing hash" do
+    inner_map[ :foo ] = 123
+    expect( subject['foo'] ).to eq( 123 )
+  end
+
+  it "reflects later changes back to the existing hash" do
+    subject[ :abc ] = 'ABC'
+    expect( inner_map[:abc] ).to eq('ABC')
+  end
+
+  it "reflects hash-type values from the existing hash as wrapped" do
+    h = {}
+    inner_map[:aaa] = h
+    expect( subject[:aaa] ).to be_kind_of( described_class )
+    expect( subject[:aaa].inner_map ).to equal( h )
+  end
+
+  it "reflects array-type values from the existing hash as wrapped" do
+    ary = []
+    inner_map[:bbb] = ary
+    expect( subject[:bbb] ).to be_kind_of( described_class::Array )
+    expect( subject[:bbb].inner_array ).to equal( ary )
+  end
+
+  it "modifies the existing underlying entry with string/symbol indifference" do
+    inner_map[ :aaa  ] = 'A'
+    inner_map[ 'bbb' ] = 'B'
+    subject[ 'aaa' ] = 'AA'
+    subject[ :bbb  ] = 'BB'
+
+    expect( inner_map ).to eq( {
+      :aaa  => 'AA',
+      'bbb' => 'BB'
+    } )
+  end
+
 end
